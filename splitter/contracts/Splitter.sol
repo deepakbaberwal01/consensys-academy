e2b9868ef54ebc29672ccfbe6c1904e5b73e2b76
dulguunbatmunkh <dulguunbatmunkh@gmail.com>
2017-09-12 19:14:42 +0900

    - no loop - no array - Send -> sendAndSplit(addr1, addr2) - added withdraw() feature - 5 tests all passing

--- splitter/contracts/Splitter.sol
+++ splitter/contracts/Splitter.sol
@@ -3,65 +3,54 @@ pragma solidity ^0.4.10;  // require added
 contract Splitter
 {
     address public owner;
+    mapping (address => uint) public  balances;
 
-    struct Balance
-    {
-      address user_address;
-      uint256 amount;
-    }
-
-    uint constant USER_LIMIT = 5;
-    Balance[5] public user_balances;
-    uint public user_balances_size;
-
-    mapping (address => bool) user_address_map;
+    event LogSplit(address sender, uint remiander, address receiver1, address receiver2, uint share_amount);
+    event LogWithdraw(address receiver);
 
     function Splitter()
     {
         owner = msg.sender;
     }
 
-    function Send() payable
+    // send and split it into 2 accounts
+    // remainder will be added to sender's account
+    function sendAndSplit(address receiver1, address receiver2)
+    payable
+    public
     returns (bool)
     {
         require(msg.value > 0);
 
-        if (user_address_map[msg.sender] == false) {  // add new user
-            require(user_balances_size <= USER_LIMIT); // limit reached
+        uint share_amount = msg.value / 2;
+        balances[receiver1] += share_amount;
+        balances[receiver2] += share_amount;
 
-            user_address_map[msg.sender] = true;
-            user_balances[user_balances_size] = Balance(msg.sender, msg.value);
-            user_balances_size++;
-        }
+        uint remainder = msg.value - share_amount*2;
+        balances[msg.sender] += remainder; // remainder refund
 
-        return Split(msg.value, msg.sender);
-    }
-
-    function Split(uint256 amount, address sender) private
-    returns (bool)
-    {
-        require(amount > 0);
-        if (user_balances_size == 1) return true; // no need to split
-
-        uint share_amount = amount / (user_balances_size - 1);
-        require(share_amount > 0);                // should be positive amount
-
-        for (uint i = 0; i < user_balances_size; i++)
-        {
-            if (user_balances[i].user_address == sender)
-            {
-                user_balances[i].amount +=
-                  (amount - share_amount*user_balances_size);  // refund remainder
-                continue;
-            }
-            user_balances[i].amount += share_amount;
-        }
+        LogSplit(msg.sender, remainder, receiver1, receiver2, share_amount);
 
         return true;
     }
 
-    function KillMe()
-    returns (bool)
+    // withdraw its amount gained by splits
+    // prevents re-entrace attack
+    function withdraw()
+    public
+    {
+        uint amount = balances[msg.sender];
+        if (amount > 0)
+        {
+            balances[msg.sender] = 0;
+            if(msg.sender.send(amount) == false) revert();
+
+            LogWithdraw(msg.sender);
+        }
+    }
+
+    function killMe()
+    public
     {
         require(msg.sender == owner);
 
--- splitter/test/splitter.js
+++ splitter/test/splitter.js
@@ -10,7 +10,7 @@ contract('Splitter', function(accounts) {
   var david = accounts[3];
 
   beforeEach(function() {
-    return Splitter.new({ from: owner })
+    return Splitter.new({from: owner})
     .then(function(instance) {
       contract = instance;
     });
@@ -24,15 +24,68 @@ contract('Splitter', function(accounts) {
       });
   });
 
-  // Kill contract and try to send amount
-  it("should kill the contract", function() {
-    return contract.KillMe({from: owner})
-      .then(function(txn) {
-          return contract.Send.call({from: alice, value: 1})
-          .then(success => {
-            assert.isFalse(success, "sent the amount to contract after killing it!");
-          });
+  it("should reject 0 amount request", function() {
+    return contract.sendAndSplit.call(bob, alice)
+      .then(success => {
+        assert.isFalse(success, "didn't reject 0 amount!");
       });
-  })
+  });
+
+  it("should split sent amount (odd -> with remainder)", function() {
+    var send_amount = web3.toWei(99, "wei");
+    return contract.sendAndSplit(bob, carol, {from: alice, value: send_amount})
+      .then(txn => {
+        return contract.balances(bob);
+      })
+      .then(balance => {
+        assert.equal(balance.toNumber(), 49, "Bob's (receiver1) balance is not correct.");
+        return contract.balances(carol);
+      })
+      .then(balance => {
+        assert.equal(balance.toNumber(), 49, "Carol's (receiver2) balance is not correct.");
+        return contract.balances(alice);
+      })
+      .then(balance => {
+        assert.equal(balance.toNumber(), 1, "Alice's (sender) balance is not correct. Remainder should have been added.");
+      });
+  });
+
+  it("should split sent amount (even -> without remainder)", function() {
+    var send_amount = web3.toWei(100, "wei");
+    return contract.sendAndSplit(bob, carol, {from: alice, value: send_amount})
+      .then(txn => {
+        return contract.balances(bob);
+      })
+      .then(balance => {
+        assert.equal(balance.toNumber(), 50, "Bob's (receiver1) balance is not correct.");
+        return contract.balances(carol);
+      })
+      .then(balance => {
+        assert.equal(balance.toNumber(), 50, "Carol's (receiver2) balance is not correct.");
+        return contract.balances(alice);
+      })
+      .then(balance => {
+        assert.equal(balance.toNumber(), 0, "Alice's (sender) balance is not empty.");
+      });
+  });
+
+  it("should withdraw and remaining balance must be 0", function() {
+    var send_amount = web3.toWei(1, "ether");
+    var carol_amount = web3.toWei(0.5, "ether");
+    return contract.sendAndSplit(bob, carol, {from: alice, value: send_amount})
+      .then(txn => {
+        return contract.balances(carol);
+      })
+      .then(balance => {
+        assert.equal(balance.toNumber(), carol_amount, "Carol's amount is not correct!");
+        return contract.withdraw({from: carol});
+      })
+      .then(txn => {
+        return contract.balances(carol);
+      })
+      .then(balance => {
+        assert.equal(balance.toNumber(), 0, "Carol's balance is not reset to 0!");
+      });
+  });
 
 });
